d1cb37f7530bb060649d350dd5f7d0fac4beef1f
Brad Graham <blg002@gmail.com>
2021-06-20 22:55:53 -0400

    Building Selectors

--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer, on } from "@ngrx/store";
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
 import * as AppState from '../../state/app.state';
 import { Product } from "../product";
 
@@ -17,6 +17,23 @@ const initialState: ProductState = {
   currentProduct: null,
   products: [],
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode
+)
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct
+)
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products
+)
 
 export const productReducer = createReducer<ProductState>(
   initialState,
