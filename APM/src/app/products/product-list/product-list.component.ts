4e8d5220d206e8ee2ed3c83aa98503db27dd8d00
Brad Graham <blg002@gmail.com>
2021-06-22 21:55:28 -0400

    Container Component

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -1,49 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-
-import { Product } from '../product';
-import { State, getShowProductCode, getCurrentProduct, getProducts, getError } from '../state/product.reducer';
-import * as ProductActions from "../state/product.actions";
-import { Observable } from 'rxjs';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+
+export class ProductListComponent {
   pageTitle = 'Products';
-  errorMessage$: Observable<string>;
-
-  displayCode$: Observable<boolean>;
-  products$: Observable<Product[]>;
-  selectedProduct$: Observable<Product>;
-
-  constructor(
-    private store: Store<State>,  
-  ) {}
-
-  ngOnInit(): void {
-    this.products$ = this.store.select(getProducts);
-    
-    this.store.dispatch(ProductActions.loadProducts());
-    
-    this.selectedProduct$ = this.store.select(getCurrentProduct);
-    this.displayCode$ = this.store.select(getShowProductCode);
-
-    this.errorMessage$ = this.store.select(getError);
-  }
-
-  checkChanged(): void {
-    this.store.dispatch(ProductActions.toggleProductCode());
-  }
-
-  newProduct(): void {
-    this.store.dispatch(ProductActions.initCurrentProduct());
-  }
-
-  productSelected(product: Product): void {
-    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
-  }
-
 }
--- APM/src/app/products/product-shell/product-shell.component.ts
+++ APM/src/app/products/product-shell/product-shell.component.ts
@@ -1,12 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Product } from '../product';
+import { State, getShowProductCode, getCurrentProduct, getProducts, getError } from '../state/product.reducer';
+import * as ProductActions from "../state/product.actions";
+import { Observable } from 'rxjs';
+
 @Component({
   templateUrl: './product-shell.component.html'
 })
+
 export class ProductShellComponent implements OnInit {
+  errorMessage$: Observable<string>;
+  displayCode$: Observable<boolean>;
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
 
-  constructor() { }
+  constructor(
+    private store: Store<State>,  
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+    this.displayCode$ = this.store.select(getShowProductCode);
+    this.errorMessage$ = this.store.select(getError);
+  }
+
+  checkChanged(): void {
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.store.dispatch(ProductActions.initCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
   }
 }
